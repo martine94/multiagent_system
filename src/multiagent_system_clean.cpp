1318d81e903071e7e165a452a45fd8f9f54ac22e
msfd <mforslund95@gmail.com>
2020-05-04 17:32:56 +0200

    olika agenter

--- src/multiagent_system_clean.cpp
+++ src/multiagent_system_clean.cpp
@@ -4,15 +4,19 @@
 #include "tf/transform_datatypes.h"
 #include "gazebo_msgs/ModelState.h"
 #include "sensor_msgs/LaserScan.h"
+#include "std_msgs/UInt16MultiArray.h"
+#include <math.h>
 
 #define M_PI 3.14159265358979323846
 #define SensorWall 0
 #define SensorBox 1
+#define angleIncrement 0.0175019223243
+#define DoNotDrive 1000.0
 
-class agent
+class Agent
 {
 public:
-    agent() {}
+    Agent() {}
     void init(std::string topicNameSub, std::string topicNamePub);
     void run();
 
@@ -24,6 +28,7 @@ protected:
     bool avoid(geometry_msgs::Twist &msg, int r);
     bool sortCan(int r);
     bool chaseCan(geometry_msgs::Twist &msg);
+    void locateBox();
 
 
     ros::Publisher velPub;
@@ -38,25 +43,59 @@ protected:
     std::vector<std::vector<float>> angles;
 
     geometry_msgs::Twist robotVel;
+    geometry_msgs::Pose robotPosition;
+    geometry_msgs::Point boxPosition;
+};
+
+class Piccolos : Agent
+{
+public:
+    void legolasCallbackLocation(const std_msgs::UInt16MultiArray::ConstPtr& msg);
+    void initPiccolos(std::string topicNameSub, std::string topicNamePub, int rID);
+    // Node handler publisher and subscriber
+    ros::Publisher bidPub;
+    ros::Subscriber boxLocationSub;
+    //ros::ServiceServer service;
+
+    int robotID;
+};
+
+class Legolas : Agent
+{
+public:
+    void locateBox();
+    void initLegolas();
+    void dealer(geometry_msgs::Pose &Picco1, geometry_msgs::Pose &Picco2);
+    void giveCommand(const geometry_msgs::Pose Picco);
+    void springLegolas();
+    void bidCallback(const geometry_msgs::Pose &robotmsg);
+
+    // Node handler publisher and subscriber
+    ros::Publisher boxLocationPub;
+    ros::Subscriber bidSub;
+
+    geometry_msgs::Pose Picco1;
+    geometry_msgs::Pose Picco2;
+    geometry_msgs::Pose Picco3;
 };
 
 
 
-void agent::init(std::string topicNameSub, std::string topicNamePub)
+void Agent::init(std::string topicNameSub, std::string topicNamePub)
 {
     distances.resize(2);
     angles.resize(2);
     std::string boxSub = topicNameSub+std::to_string(2);
 
-    wallLaserSub = n.subscribe(topicNameSub, 2, &agent::sensorCallbackWall,this);
-    boxLaserSub = n.subscribe(boxSub, 2, &agent::sensorCallbackBox,this);
+    wallLaserSub = n.subscribe(topicNameSub, 2, &Agent::sensorCallbackWall,this);
+    boxLaserSub = n.subscribe(boxSub, 2, &Agent::sensorCallbackBox,this);
     velPub = n.advertise<geometry_msgs::Twist>(topicNamePub, 3);
 
 }
 
 
 
-void agent::sensorCallbackWall(const sensor_msgs::LaserScan::ConstPtr& msg)
+void Agent::sensorCallbackWall(const sensor_msgs::LaserScan::ConstPtr& msg)
 {
     // Clear data from previous scan reading.
     this->distances[SensorWall].clear();
@@ -80,7 +119,7 @@ void agent::sensorCallbackWall(const sensor_msgs::LaserScan::ConstPtr& msg)
 
 
 
-void agent::sensorCallbackBox(const sensor_msgs::LaserScan::ConstPtr& msg)
+void Agent::sensorCallbackBox(const sensor_msgs::LaserScan::ConstPtr& msg)
 {
     // Clear data from previous scan reading.
     this->distances[SensorBox].clear();
@@ -103,7 +142,7 @@ void agent::sensorCallbackBox(const sensor_msgs::LaserScan::ConstPtr& msg)
 
 
 
-float agent::regionDistance(const float &start, const float &stop, int r)
+float Agent::regionDistance(const float &start, const float &stop, int r)
 {
     int i = 0, j = 0;
 
@@ -123,7 +162,7 @@ float agent::regionDistance(const float &start, const float &stop, int r)
 
 
 
-void agent::checkDirection(std::vector<float>& sensorValues, int r)
+void Agent::checkDirection(std::vector<float>& sensorValues, int r)
 {
     float minRight = regionDistance(M_PI*(12.0/8.0), M_PI*(14.0/8.0), r);
     float minRightFront = regionDistance(M_PI*(14.0/8.0), M_PI*(15.5/8.0), r);
@@ -144,7 +183,7 @@ void agent::checkDirection(std::vector<float>& sensorValues, int r)
 
 
 
-bool agent::avoid(geometry_msgs::Twist &msg, int r)
+bool Agent::avoid(geometry_msgs::Twist &msg, int r)
 {
     std::vector<float> sensorBox(6);
     checkDirection(sensorBox, r);
@@ -255,7 +294,7 @@ bool agent::avoid(geometry_msgs::Twist &msg, int r)
 
 
 
-bool agent::sortCan(int r)
+bool Agent::sortCan(int r)
 {
     std::vector<float> sensorWall(6);
     checkDirection(sensorWall, SensorWall);
@@ -286,7 +325,7 @@ bool agent::sortCan(int r)
 
 
 
-bool agent::chaseCan(geometry_msgs::Twist &msg)
+bool Agent::chaseCan(geometry_msgs::Twist &msg)
 {
     float direction = 0.0;
     std::vector<float> sensorWall(6), sensorBox(6);
@@ -380,7 +419,150 @@ bool agent::chaseCan(geometry_msgs::Twist &msg)
 
 
 
-void agent::run()
+void Legolas::initLegolas()
+{
+    bidSub = n.subscribe("/Piccolo/bids/", 2, &Legolas::bidCallback,this);
+    boxLocationPub = n.advertise<std_msgs::UInt16MultiArray>("/Legolas/BoxLocation/", 3);
+}
+
+
+
+void Legolas::bidCallback(const geometry_msgs::Pose &robotmsg)
+{
+    if(robotmsg.orientation.x == 1)
+        Picco1 = robotmsg;
+    else if(robotmsg.orientation.x == 2)
+        Picco2 = robotmsg;
+    else if(robotmsg.orientation.x == 3)
+        Picco3 = robotmsg;
+}
+
+
+
+void Legolas::locateBox()
+{
+    int i = 0;
+    int j = 0;
+    int count = 0;
+    float t = robotPosition.orientation.z;
+
+
+    for(float distance:distances[SensorBox])
+    {
+        if (distance < 2.0)
+            if(distances[SensorWall][j] > distance + 0.3)
+                i++;
+        j++;
+    }
+
+    std::vector<uint16_t> items;
+    std_msgs::UInt16MultiArray foundBoxes;
+    j = 0;
+    for(float distance:distances[SensorBox])
+    {
+        if (distance < 2.0)
+         {
+            if(distances[SensorWall][j] > distance + 0.3)
+            {
+                int y = sin((j*angleIncrement)+t)*distance;
+                int x = cos((j*angleIncrement)+t)*distance;
+                items.push_back( x );
+                count++;
+                items.push_back( y );
+                count++;
+            }
+         }
+        j++;
+    }
+
+    foundBoxes.data = items;
+
+    if(i > 0)
+        this->boxLocationPub.publish(foundBoxes);
+
+}
+
+void Legolas::dealer(geometry_msgs::Pose &Picco1, geometry_msgs::Pose &Picco2)
+{
+    if(Picco1.orientation.z == Picco2.orientation.z)
+        if(Picco1.orientation.y == std::min(Picco1.orientation.y,Picco2.orientation.y))
+            Picco2.orientation.y = DoNotDrive;
+        else if(Picco2.orientation.y == std::min(Picco1.orientation.y,Picco2.orientation.y))
+            Picco1.orientation.y = DoNotDrive;
+}
+
+
+
+void Legolas::giveCommand(const geometry_msgs::Pose Picco)
+{
+    if(Picco.orientation.y != DoNotDrive)
+    {
+        std::string topicName = "/orderToPiccolo"+std::to_string(Picco.position.x);
+        ros::Publisher currPicc;
+        geometry_msgs::Pose destination;
+        destination.position.x = Picco.position.x;
+        destination.position.y = Picco.position.y;
+        currPicc = this->n.advertise<geometry_msgs::Pose>(topicName,3);
+        currPicc.publish(destination);
+    }
+}
+
+
+
+void Legolas::springLegolas()
+{
+    this->locateBox();
+    this->dealer(this->Picco1, this->Picco2);
+    this->dealer(this->Picco1, this->Picco3);
+    this->dealer(this->Picco2, this->Picco3);
+    this->giveCommand(this->Picco1);
+    this->giveCommand(this->Picco2);
+    this->giveCommand(this->Picco3);
+}
+
+
+
+void Piccolos::initPiccolos(std::string topicNameSub, std::string topicNamePub, int rID)
+{
+    this->boxLocationSub = this->n.subscribe(topicNameSub, 2,&Piccolos::legolasCallbackLocation,this);
+    this->bidPub = this->n.advertise<geometry_msgs::Pose>(topicNamePub, 3);
+    this->robotID = rID;
+    //this->service = n.advertiseService("add_two_ints", add);
+}
+
+void Piccolos::legolasCallbackLocation(const std_msgs::UInt16MultiArray::ConstPtr& msg)
+{
+    geometry_msgs::Pose bid;
+    int minDist = 10000;
+    int minIndex;
+    float currDist;
+    for(int i = 0; i < msg->data.size();i+2)
+    {
+        currDist = std::sqrt(std::pow(this->robotPosition.position.x -  msg->data[i],2)+std::pow(this->robotPosition.position.y -  msg->data[i+1],2));
+        if(currDist < minDist)
+        {
+            minDist = currDist;
+            minIndex = i;
+        }
+
+    }
+    bid.orientation.x = this->robotID;
+    if(robotVel.linear.x != 0.0)
+        bid.orientation.y = DoNotDrive;
+    else
+        bid.orientation.y = currDist;
+    bid.orientation.z = minIndex;
+    bid.position.x = msg->data[minIndex];
+    bid.position.y = msg->data[minIndex+1];
+    // skicka med robot-id, box-index, score
+    //Ska publisera på slutet av sig
+    this->bidPub.publish(bid);
+}
+
+
+
+
+void Agent::run()
 {
     this->robotVel.linear.x =0.4;
     this->robotVel.angular.z = 0.3;
@@ -396,19 +578,26 @@ int main(int argc, char **argv) {
 
   // Init the connection with the ROS system
   ros::init(argc, argv, "lab1_node");
-  agent Robot1, Robot2, Robot3;
-  Robot1.init("/robot1/scan", "/robot1/cmd_vel");
-  Robot2.init("/robot2/scan", "/robot2/cmd_vel");
-  Robot3.init("/robot3/scan", "/robot3/cmd_vel");
+  //Agent Robot1, Robot2, Robot3, Robot4;
+  //Robot1.init("/robot1/scan", "/robot1/cmd_vel");
+  //Robot2.init("/robot2/scan", "/robot2/cmd_vel");
+  //Robot3.init("/robot3/scan", "/robot3/cmd_vel");
+  //Robot4.init("/robot4/scan", "/robot4/cmd_vel");
 
+  Legolas scout;
+  scout.initLegolas();
+
+  Piccolos slave1, slave2, slave3;
+  slave1.initPiccolos("/Legolas/BoxLocation/", "/Piccolo/bids/",1);
+  slave2.initPiccolos("/Legolas/BoxLocation/", "/Piccolo/bids/",2);
+  slave3.initPiccolos("/Legolas/BoxLocation/", "/Piccolo/bids/",3);
   // Start the ROS main loop
   ros::Rate loop_rate(100);
   while (ros::ok()) {
-    Robot1.run();
-    Robot2.run();
-    Robot3.run();
-
-
+    //Robot1.run();
+    //Robot2.run();
+    //Robot3.run();
+    //Robot4.run();
 
     ros::spinOnce();
     loop_rate.sleep();
